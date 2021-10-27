3375d2dcca7a6b6f764440799e1f28f17f5d153a
David Silveira <daviewave@gmail.com>
2021-10-27 02:39:48 -0400

    updated script.js so now when the user presses the enter button next to the input area for their name, whatever string is in the input box at the time of the button press will be saved

--- assets/script.js
+++ assets/script.js
@@ -4,15 +4,19 @@
 var getNameInputBtn = document.getElementById("button-addon2");
 
 //DATA
+//need to store name of current user after they enter it in getNameInput event listenter
+var currentUserName = "";
 
 //FUNCTIONS
 
 //USER INTERACTIONS
 //TODO: save the users input when they are prompted at the very start of the page
-getNameInput.addEventListener("click", function (event) {
+getNameInputBtn.addEventListener("click", function (event) {
   event.preventDefault();
 
   //TODO: write code so that when user presses button to submit their name, if something has been entered in the input area, we need to save the name and display it to the span where the text is like "Hello ____! Welcome to Foode!"
   //targets the <input> for the user
   var $userNameInput = $("#name-input");
+  currentUserName = $userNameInput.val();
+  console.log(currentUserName);
 });
