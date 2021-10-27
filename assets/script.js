f0e3fdf9b63fe9feea9394608be91d5b7cfd970f
David Silveira <daviewave@gmail.com>
2021-10-27 02:33:49 -0400

    updated script.js by moving the location of variable userNameInput to inside the event listener function because that is the only place it will be used (save memory)

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,4 @@
 //DEPENDENCIES
-//targets the <input> for the user
-var $userNameInput = $("#name-input");
 
 //targets the button the users presses to enter their name at the top of the page
 var getNameInputBtn = document.getElementById("button-addon2");
@@ -15,4 +13,6 @@ getNameInput.addEventListener("click", function (event) {
   event.preventDefault();
 
   //TODO: write code so that when user presses button to submit their name, if something has been entered in the input area, we need to save the name and display it to the span where the text is like "Hello ____! Welcome to Foode!"
+  //targets the <input> for the user
+  var $userNameInput = $("#name-input");
 });
