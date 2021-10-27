c9f0e50670b274ff058511b87e37b3826f420e42
David Silveira <daviewave@gmail.com>
2021-10-27 02:23:01 -0400

    updated script.js by adding a variable to target the button the user presses after they have typed their name into the input area at top of page

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,12 @@
 //targets the <input> for the user
 var $userNameInput = $("#name-input");
 
+//targets the button the users presses to enter their name at the top of the page
+var getNameInput = document.getElementById("button-addon2");
+
 //DATA
 
 //FUNCTIONS
 
 //USER INTERACTIONS
+//TODO: save the users input when they are prompted at the very start of the page
