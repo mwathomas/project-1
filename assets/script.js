3fee5237c7aa47014117fdd156a2a252645107a8
M Abrams <madeline.a.thomas@gmail.com>
2021-10-27 12:14:38 -0400

    added ul to html and ability to add ingredients to list on page

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,7 @@
 //targets the button the users presses to enter their name at the top of the page
 var getNameInputBtn = document.getElementById("button-addon2");
 var getIngredientInputBtn = document.getElementById("submit-ingredient-btn");
+var getIngredientInputEl = document.getElementById("add-ingredient-input");
 
 //DATA
 //need to store name of current user after they enter it in getNameInput event listenter
@@ -13,10 +14,12 @@ var ingredientsArr = [];
 //uses the name the user entered to display a message unique to the current user
 function welcomeUser() {
   //variable to target span to add user's name to
+  document.getElementById("name-input-box").setAttribute("hidden", true);
   var $welcomeUserMessageTarget = $("#user-name"); //document.getElementById("user-name");
   $welcomeUserMessageTarget.text(
     "Hello, " + currentUserName + "! Welcome to FoodE!"
   );
+
   askUserForIngredients();
 }
 
@@ -52,6 +55,13 @@ function checkUserInput(string) {
 
 //TODO: WRITE FUNCTION TO SHOW THE CURRENT LIST OF SEARCHED INGREDIENTS IN 'list-of-entered-ingredients-container'
 function renderIngredientsOnSearchList() {
+  var ingredientsList = [];
+  var ingredientAdd = getIngredientInputEl.value;
+  var li = document.createElement("li");
+  li.append(ingredientAdd);
+  ingredientsList.push(ingredientAdd);
+  document.getElementById("ingredients-list").appendChild(li);
+
   /*
     //loop through each item in the array until the end
         //we want to create a new little 'section' (<div>) that we can add both a <p> or <h1> to AND a button that  the user will be able to press to remove an element from their list to search
@@ -92,22 +102,6 @@ getNameInputBtn.addEventListener("click", function (event) {
 //TODO: When the user clicks the 'Add Ingredient' button write function that adds each ingredient to an array/might need local storage
 getIngredientInputBtn.addEventListener("click", function (event) {
   event.preventDefault();
-
   //this targets the input from the add ingredients part of the process
-  var $currentIngredientAdded = $("#add-ingredient-input");
-  var currentIngredientString = $currentIngredientAdded.val().trim();
-
-  //this clears the last searched ingredient from the input area so another ingredient can be entered without having to delete your last search
-  var clearLastSearchedIngredient = document.getElementById(
-    "add-ingredient-input"
-  );
-  clearLastSearchedIngredient.value = "";
-
-  //checks to make sure there was something in input area when submit button is pressed
-  checkUserInput(currentIngredientString);
-
-  //add the string to the 'ingredientsArr' array that is storing every ingredient added
-  ingredientsArr.push(currentIngredientString);
-  //TODO: write a function that will display the array of searched ingredients INTO THE 'list-of-entered-ingredients-container'
-  //renderIngredientsOnSearchList();
+  renderIngredientsOnSearchList();
 });
