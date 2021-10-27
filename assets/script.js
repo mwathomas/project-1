19311efa92124281510773a9ceb62777f81395c6
David Silveira <daviewave@gmail.com>
2021-10-27 02:31:35 -0400

    updated script.js by adding an event listenter for when the user 'clicks' on the enter button next to the input area for their name -- function currently only prevents default, code has not been written but a TODO: describes what the function needs to do

--- assets/script.js
+++ assets/script.js
@@ -11,3 +11,8 @@ var getNameInputBtn = document.getElementById("button-addon2");
 
 //USER INTERACTIONS
 //TODO: save the users input when they are prompted at the very start of the page
+getNameInput.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  //TODO: write code so that when user presses button to submit their name, if something has been entered in the input area, we need to save the name and display it to the span where the text is like "Hello ____! Welcome to Foode!"
+});
