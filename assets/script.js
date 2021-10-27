50ba9d6173001833524ff30ae337f6c88daa077b
David Silveira <daviewave@gmail.com>
2021-10-27 05:51:41 -0400

    updated script.js by adding an event listener for the add ingredient button that gets and stores the string the user inputs (as long as input entered isnt empty) and adds it to the array 'ingredientsArr' which will then be used in a function to display them to the user and let them remove

--- assets/script.js
+++ assets/script.js
@@ -27,7 +27,6 @@ function askUserForIngredients() {
 
   //variable to target span to add user's name to
   var $askUserIngredientsTarget = $("#ask-user-ingredients"); //document.getElementById("user-name");
-  console.log($askUserIngredientsTarget.val());
   $askUserIngredientsTarget.text(
     "What ingredients will you be cooking with today, " + currentUserName
   );
@@ -40,14 +39,14 @@ function displayIngredientAddInputAbility() {
 }
 
 //USER INTERACTIONS
-//TODO: save the users input when they are prompted at the very start of the page
+//DONE: save the users input when they are prompted at the very start of the page
 getNameInputBtn.addEventListener("click", function (event) {
   event.preventDefault();
 
-  //TODO: write code so that when user presses button to submit their name, if something has been entered in the input area, we need to save the name and display it to the span where the text is like "Hello ____! Welcome to Foode!"
+  //DONE: write code so that when user presses button to submit their name, if something has been entered in the input area, we need to save the name and display it to the span where the text is like "Hello ____! Welcome to Foode!"
   //targets the <input> for the user
   var $userNameInput = $("#name-input");
-  currentUserName = $userNameInput.val();
+  currentUserName = $userNameInput.val().trim();
 
   //checks to make sure there was something in input area when submit button is pressed
   if (currentUserName === "") {
@@ -58,4 +57,31 @@ getNameInputBtn.addEventListener("click", function (event) {
   }
   //call a function that adds the users name to the <span> in the header welcoming them
   welcomeUser();
+});
+
+//TODO: When the user clicks the 'Add Ingredient' button write function that adds each ingredient to an array/might need local storage
+getIngredientInputBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  //this targets the input from the add ingredients part of the process
+  var $currentIngredientAdded = $("#add-ingredient-input");
+  var currentIngredientString = $currentIngredientAdded.val().trim();
+
+  //this clears the last searched ingredient from the input area so another ingredient can be entered without having to delete your last search
+  var clearLastSearchedIngredient = document.getElementById(
+    "add-ingredient-input"
+  );
+  clearLastSearchedIngredient.value = "";
+
+  //checks to make sure there was something in input area when submit button is pressed
+  if (currentIngredientString === "") {
+    alert("ERROR: NO INGREDIENT ENTERED");
+    return;
+  }
+
+  //add the string to the 'ingredientsArr' array that is storing every ingredient added
+  ingredientsArr.push(currentIngredientString);
+  console.log(ingredientsArr);
+  //TODO: write a function that will display the array of searched ingredients INTO THE 'list-of-entered-ingredients-container'
+  //renderIngredientsOnSearchList();
 });
